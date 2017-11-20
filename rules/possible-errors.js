1ae2573bd2dcbacba12d928788e8e12c21767706
x0r2 <x0r2>
2017-11-20 16:56:36 +0300

    Add possible errors

---
+++ rules/possible-errors.js
@@ -0,0 +1,40 @@
+module.exports = {
+    'for-direction': 'error',
+    'getter-return': 'error',
+    'no-await-in-loop': 'error',
+    'no-compare-neg-zero': 'error',
+    'no-cond-assign': ['error', 'always'],
+    'no-console': 'warn',
+    'no-constant-condition': 'warn',
+    'no-control-regex': 'error',
+    'no-debugger': 'error',
+    'no-dupe-args': 'error',
+    'no-dupe-keys': 'error',
+    'no-duplicate-case': 'error',
+    'no-empty': 'error',
+    'no-empty-character-class': 'error',
+    'no-ex-assign': 'error',
+    'no-extra-boolean-cast': 'error',
+    'no-extra-parens': 'error',
+    'no-extra-semi': 'error',
+    'no-func-assign': 'error',
+    'no-inner-declarations': ['error', 'both'],
+    'no-invalid-regexp': 'error',
+    'no-irregular-whitespace': ['error', {
+        'skipStrings': false
+    }],
+    'no-obj-calls': 'error',
+    'no-prototype-builtins': 'error',
+    'no-regex-spaces': 'error',
+    'no-sparse-arrays': 'error',
+    'no-template-curly-in-string': 'error',
+    'no-unexpected-multiline': 'error',
+    'no-unreachable': 'error',
+    'no-unsafe-finally': 'error',
+    'no-unsafe-negation': 'error',
+    'use-isnan': 'error',
+    'valid-jsdoc': 'off',
+    'valid-typeof': ['error', {
+        'requireStringLiterals': true
+    }]
+};
