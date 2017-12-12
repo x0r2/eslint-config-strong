2725c5e2fd1e8787de1f495b3a7561dc0e79124b
x0r2 <x0r2>
2017-12-12 17:00:35 +0300

    Add node rules

---
+++ rules/node.js
@@ -0,0 +1,13 @@
+module.exports = {
+    'callback-return': 'error',
+    'global-require': 'error',
+    'handle-callback-err': 'error',
+    'no-buffer-constructor': 'error',
+    'no-mixed-requires': ['error', true],
+    'no-new-require': 'error',
+    'no-path-concat': 'error',
+    'no-process-env': 'error',
+    'no-process-exit': 'off',
+    'no-restricted-modules': 'off',
+    'no-sync': 'error'
+};
