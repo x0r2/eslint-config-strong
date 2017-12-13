8b5e6c5e44bf79c9c500eed8fb94ab3a723ebd89
x0r2 <x0r2>
2017-12-13 17:20:19 +0300

    Add index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+module.exports = {
+    extends: [
+        './rules/possible-errors',
+        './rules/best-practices',
+        './rules/strict-mode',
+        './rules/variables',
+        './rules/node',
+        //'./rules/stylistic-issues',
+        //'./rules/es6'
+    ].map(require.resolve),
+
+    parserOptions: {
+        ecmaVersion: 2017,
+        sourceType: 'module',
+        ecmaFeatures: {
+            experimentalObjectRestSpread: true
+        }
+    }
+};
